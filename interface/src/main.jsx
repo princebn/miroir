21d879f750bc67187380fb10305ae7f8582eedad
Prince Nehemie <nbikuka@albertschool.com>
2026-06-11 10:45:47 +0200

    Interface: polices auto-hebergees (fontsource) + correctif lancement dev (stdin detache)

--- interface/src/main.jsx
+++ interface/src/main.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "@fontsource/fraunces/400.css";
+import "@fontsource/fraunces/400-italic.css";
+import "@fontsource/fraunces/500.css";
+import "@fontsource/fraunces/500-italic.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(<App />);
